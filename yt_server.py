6ba262642b503220d74a3be46b4c14bb74c4077c
amerpro1 <amer16.express@gmail.com>
2025-10-05 23:29:29 +0300

    Update yt_server.py

--- yt_server.py
+++ yt_server.py
@@ -59,3 +59,8 @@ def download():
 
         return send_file(file_path, as_attachment=True)
     except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
